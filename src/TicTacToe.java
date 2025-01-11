04d6195f0a320500f22f15e0a267a231d18b7438
Nadun-Charuka <nadun.charuka2016@gmail.com>
2025-01-11 09:54:18 +0530

    top bar set

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,8 +8,30 @@ public class TicTacToe implements ActionListener{
     JFrame frame = new JFrame();
     JPanel title_panel = new JPanel();
     JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean palyer1_turn;
 
-    public void ticTacToe(){
+    public TicTacToe(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        frame.getContentPane().setBackground(new Color(50,50,50));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        textfield.setBackground(new Color(25,25,25));
+        textfield.setForeground(new Color(25,255,0));
+        textfield.setFont(new Font("Ink Free",Font.BOLD,75));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("Tic - Tac - Toe");
+        textfield.setOpaque(true);
+
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0,0,800,100);
+
+        title_panel.add(textfield);
+        frame.add(title_panel,BorderLayout.NORTH);
+
 
     };
 
